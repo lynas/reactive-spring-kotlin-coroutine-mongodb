3acc4d32d22d8c039edd544bc1e7c7c40796b9be
Md Sazzad Islam <szlynas@gmail.com>
2019-11-28 15:30:16 +0600

    Update ReactiveSpringKotlinMongoApplication.kt

--- src/main/kotlin/com/lynas/reactivespringkotlinmongo/ReactiveSpringKotlinMongoApplication.kt
+++ src/main/kotlin/com/lynas/reactivespringkotlinmongo/ReactiveSpringKotlinMongoApplication.kt
@@ -27,6 +27,20 @@ fun main(args: Array<String>) {
 
 @Document
 data class Customer(@Id val id: String? = null, val firstName: String, val lastName: String)
+@Document
+data class Order(@Id val id: String? = null, val customerId: String, val orderItemName: String)
+
+@Service
+class OrderService(val operation: ReactiveMongoOperations) {
+    suspend fun insert(order: Order): Order? {
+        return operation.insert(order).awaitSingle()
+    }
+
+    suspend fun findByCustomerId(customerId: String): List<Order> {
+        return operation.find<Order>(Query(where("customerId").isEqualTo(customerId))).asFlow().toList()
+    }
+}
+
 
 @Service
 class CustomerService(val operation: ReactiveMongoOperations) {
@@ -39,15 +53,18 @@ class CustomerService(val operation: ReactiveMongoOperations) {
         return operation.findAll<Customer>().asFlow().toList()
     }
 
+    suspend fun findOneById(customerId: String): Customer {
+        return operation.find<Customer>(Query(where("_id").isEqualTo(customerId))).awaitSingle()
+    }
+
     suspend fun findByLastName(lastName: String): Customer? {
         return operation.find<Customer>(Query(where("lastName").isEqualTo(lastName))).awaitSingle()
-
     }
 }
 
 @RestController
 @RequestMapping("/customers")
-class CustomerRestController(val customerService: CustomerService) {
+class CustomerRestController(val customerService: CustomerService, val orderService: OrderService) {
 
     @GetMapping
     suspend fun getAll(): List<Customer> {
@@ -62,6 +79,13 @@ class CustomerRestController(val customerService: CustomerService) {
     @PostMapping
     suspend fun addNew(@RequestBody customer: Customer): Customer? {
         return customerService.insert(customer)
+    }
+
+    @GetMapping("/orders/{customerId}")
+    suspend fun getCustomerOrders(@PathVariable customerId: String): List<Order> {
+        val customer = customerService.findOneById(customerId)
+        return orderService.findByCustomerId(customer.id
+                ?: throw RuntimeException("customer not found with customerId: $customerId"))
     }
 
 
